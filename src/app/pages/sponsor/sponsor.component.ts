0a7c8caef2ce9812b4a733cab8ec52552c1d9552
Noril Werlen <noril.werlen@pm.me>
2022-12-24 23:07:21 -0500

    added files

---
+++ src/app/pages/sponsor/sponsor.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sponsor',
+  templateUrl: './sponsor.component.html',
+  styleUrls: ['./sponsor.component.css']
+})
+export class SponsorComponent {
+
+}
