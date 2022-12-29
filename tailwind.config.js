213d956f7a3aff91eb0e1c8a22aafdb3becdf8dd
Noril Werlen <noril.werlen@pm.me>
2022-12-28 19:58:35 -0500

    banner update

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,16 @@ module.exports = {
         'bg1': 'url("https://i.ibb.co/NpkK7Cn/bg-1.jpg")',
         'ima2': 'url("https://www.team3990.com/wp-content/uploads/2017/08/21556923_10154616737675566_1187701955_o.jpg")',
         'ima3': 'url("https://www.team3990.com/wp-content/uploads/2017/08/21556923_10154616737675566_1187701955_o.jpg")',
+        'banner1': 'url("https://www.team3990.com/wp-content/uploads/2017/08/infiniterecharge-290x300.png")',
+        'banner2': 'url("https://www.team3990.com/wp-content/uploads/2017/08/logoetbannieres2019-1-291x300.png")',
+        'banner3': 'url("https://www.team3990.com/wp-content/uploads/2017/08/2018Banner.png")',
+        'banner4': 'url("https://www.team3990.com/wp-content/uploads/2017/08/game2017.png")',
+        'banner5': 'url("https://www.team3990.com/wp-content/uploads/2017/08/game2016-1.png")',
+        'banner6': 'url("https://www.team3990.com/wp-content/uploads/2017/08/game2017-1.png")',
+        'banner7': 'url("https://www.team3990.com/wp-content/uploads/2017/08/game2014-1.png")',
+        'banner8': 'url("https://www.team3990.com/wp-content/uploads/2017/08/game2017-2.png")',
+        'banner9': 'url("https://www.team3990.com/wp-content/uploads/2017/08/game2012.png")',
+
       }
     },
   },
