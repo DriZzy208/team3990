b8d5e1f1354e1548874801495002217cca88d0bc
Noril Werlen <noril.werlen@pm.me>
2023-01-18 15:11:51 -0500

    season 2020

--- src/app/pages/season2020/season2020.component.ts
+++ src/app/pages/season2020/season2020.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-
+import { AboutComponent } from 'src/app/pages/about/about.component';
 @Component({
   selector: 'app-season2020',
   templateUrl: './season2020.component.html',
   styleUrls: ['./season2020.component.css']
 })
 export class Season2020Component {
+  about = new AboutComponent();
 
 }
