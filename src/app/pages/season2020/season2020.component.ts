5c6dca67c3e88d2e53646188ccaceba5e4b43505
Noril Werlen <noril.werlen@pm.me>
2023-01-24 14:50:15 -0500

    season update

--- src/app/pages/season2020/season2020.component.ts
+++ src/app/pages/season2020/season2020.component.ts
@@ -1,7 +1,8 @@
 import { AboutComponent } from 'src/app/pages/about/about.component';
 import { Component } from '@angular/core';
 import * as data from '../../../assets/seasons/2020/students.json';
-import * as sus from '../../../assets/seasons/2020/mentors.json';
+import * as hey from '../../../assets/seasons/2020/mentors.json';
+import * as doc from '../../../assets/seasons/2020/photos.json';
 @Component({
   selector: 'app-season2020',
   templateUrl: './season2020.component.html',
@@ -10,7 +11,7 @@ import * as sus from '../../../assets/seasons/2020/mentors.json';
 export class Season2020Component {
   about = new AboutComponent();
   students: any = (data as any).default;
-  mentors: any = (sus as any).default;
-
+  mentors: any = (hey as any).default;
+  photos: any = (doc as any).default;
   
 }
